9c7118f5c93920e2427a98c0de4d9512b32098d2
Patrick Buechler <buechler.pat@gmail.com>
2016-06-13 16:29:42 -0500

    Almost have login page working.

---
+++ Database/RealmDB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Realms;
+using System.ServiceModel.Security;
+
+namespace BasicScanner
+{
+	public class RealmDB
+	{
+		public RealmDB()
+		{
+		}
+
+		public class User : RealmObject { 
+
+			[ObjectId]
+			public string username { get; set; }
+
+			public string password { get; set; }
+		
+		}
+	}
+}
+
--- Views/LoginPageViewModel.cs
+++ Views/LoginPageViewModel.cs
@@ -1,9 +1,42 @@
 ﻿using System;
+using Xamarin.Forms;
+using Realms;
+using System.Linq;
+
 namespace BasicScanner
 {
-	public enum LoginPageViewModel
+	public class LoginPageViewModel
 	{
-		
+		private Realm _realm;
+		LoginPage page = new LoginPage();
+		public LoginPageViewModel() {
+			
+		}
+
+		public async void Login(string userParam, string passParam)
+		{
+
+			_realm = Realm.GetInstance();
+			var currentUser = _realm.All<RealmDB.User>().Where(d => (d.username == userParam)).ToList().FirstOrDefault();
+			if (currentUser == null)
+			{
+				var answer = await page.DisplayAlert("Login Failed", "Would you like to create a user with the username " + userParam + "?", "Yes", "No");
+				if (answer == true)
+				{
+					_realm.Write(() =>
+					{
+						var newUser = _realm.CreateObject<RealmDB.User>();
+						newUser.username = userParam;
+						newUser.password = passParam;
+					});
+				}
+				else{
+					return;
+				}
+
+			}
+		}
+
 	}
 }
 
