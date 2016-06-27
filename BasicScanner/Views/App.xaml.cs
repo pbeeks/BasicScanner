440fd17f9b42c7a9214b04b84209aa8848a6c29d
Patrick Buechler <buechler.pat@gmail.com>
2016-06-27 14:54:45 -0500

    Working on small improvements.

--- BasicScanner/Views/App.xaml.cs
+++ BasicScanner/Views/App.xaml.cs
@@ -4,7 +4,7 @@ namespace BasicScanner
 {
 	public partial class App : Application
 	{
-		public RealmDB.User currentUser { get; set; }
+		public RealmDB.User goob { get; set; }
 
 		public App()
 		{
